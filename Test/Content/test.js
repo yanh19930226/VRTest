691714e48633eed04bce2663ed0b2c6f2482dd81
891367701@qq.com <891367701@qq.com>
2019-08-13 22:57:17 +0800

    添加项目文件。

---
+++ Test/Content/test.js
@@ -0,0 +1,167 @@
+﻿var imgpath = window.document.location.href + "/Krpano/vtour/";
+
+//0:小行星开场,1:特效,2:自动旋转,
+//var datastring = "@ViewBag.Data";
+
+$(function () {
+    initPano();
+});
+
+var initPano = function () {
+    //ajax异步后台获取数据
+    var settings = {};
+    //#region 待确认方法
+    //感觉这两个不是自定义的效果
+    settings["events[skin_events].onloadcomplete"] = "skin_showloading(false);";
+    settings["onstart"] = '';
+    //#endregion
+
+    //#region 根据数据动态设置是否开始时弹出场景选择(后台Switch按钮)
+    //if (data.scenechoose=='1') {
+    //    settings["events[skin_events].onloadcomplete"] += "open_show_scene_thumb();";
+    //}
+    settings["events[skin_events].onloadcomplete"] += "open_show_scene_thumb();";
+    //#endregion
+
+    //#region 根据数据动态设置自动旋转和小行星开场(后台Switch按钮)
+    //settings['skin_settings.littleplanetintro'] = data.littleplanet=="1" ? true : false;
+    //settings['autorotate.enabled'] = data.autorotate == "1" ? true : false;
+
+    settings['skin_settings.littleplanetintro'] = true;
+    settings['autorotate.enabled'] = true;
+    //#endregion
+
+    //#region 根据数据动态设置是否显示多场景(后台Switch按钮)
+    //if (data.scene_group.sceneGroups.length>0) {
+    //     $(".vrshow_container_3_min .img_desc_container_min:eq(0) img").attr('src',data.scene_group.sceneGroups[0].imgPath);
+    //}
+    $(".vrshow_container_3_min .img_desc_container_min:eq(0) img").attr('src', '/Krpano/vtour/panos/IMG_1191-HDR_Panorama.tiles/thumb.jpg');
+    //#endregion
+    embedpano({
+        swf: "/Krpano/vtour/tour.swf",
+        xml: "/Krpano/vtour/tour.xml",
+        target: "pano",
+        html5: "auto",
+        mobilescale: 1.0,
+        passQueryParameters: true,
+        vars: settings
+    });
+};
+
+
+//Krpan调用初始化高级设置(包括特效设置、沙盘图、视角设置)
+function initAdvancedSetting(scenename) {
+    initeffect();
+};
+
+var initlittleplanet = function (scenename) {
+    var krpano = document.getElementById('krpanoSWFObject');
+    var little = datastring.split(",")[0];
+    if (little === "true") {
+        krpano.call('skin_setup_littleplanetintro(5,-90,90,"0.0");');
+    }
+};
+var initeffect = function () {
+    var krpano = document.getElementById('krpanoSWFObject');
+    if (datastring === "") {
+        alert("初始化没有设置效果!!!");
+    } else {
+        alert("加载效果...");
+        //var effectstr = datastring.split(",")[1];
+        var effectstr = "money";
+        krpano.call('addEffect("' + effectstr + '","' + imgpath + '")');
+    }
+};
+//热点初始化
+var initHotspotSetting = function () {
+
+};
+//沙盘图初始化
+var initSandTableSetting = function () {
+
+};
+//Krpan调用判断场景数量显示场景选择按钮
+var showPanoBtns = function (sceneCount) {
+    //    if (sceneCount > 1) {
+    //    $(".vrshow_container_3_min .img_desc_container_min:eq(0)").show();
+    //} else {
+    //    $(".vrshow_container_3_min .img_desc_container_min:eq(0)").hide();
+    //}
+    $(".vrshow_container_3_min .img_desc_container_min:eq(0)").show();
+    $("#panoBtns").show();
+};
+var save = function () {
+    var formData = $("#test").serializeArray();
+    $.ajax({
+        type: "POST",
+        dataType: 'json',
+        url: "/Home/Save",
+        data: formData,
+        success: function (result) {
+            window.location.reload();
+        }
+    });
+};
+
+//#region 页面点击操作方法:是否进入VR、是否全屏、背景音乐控制、背景解说
+//#region 是否进入VR
+function showWebVR() {
+    var krpano = document.getElementById('krpanoSWFObject');
+    var webvr = krpano.get("webvr");
+    webvr.entervr();
+};
+//#endregion
+
+//#region 屏幕设置
+function fullscreen(el) {
+    if ($(el).hasClass('btn_fullscreen')) {
+        launchFullScreen(document.getElementById('fullscreenid'));
+        var krpano = document.getElementById('krpanoSWFObject');
+        krpano.call("skin_showthumbs(false);");
+    } else {
+        exitFullscreen();
+    }
+    toggleFullscreenBtn(el);
+};
+
+function launchFullScreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+};
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+};
+
+function toggleFullscreenBtn(el) {
+    if ($(el).hasClass("btn_fullscreen")) {
+        $(el).removeClass("btn_fullscreen");
+        $(el).addClass("btn_fullscreen_off");
+    } else {
+        $(el).removeClass("btn_fullscreen_off");
+        $(el).addClass("btn_fullscreen");
+    }
+};
+
+
+//#endregion
+function showthumbs() {
+    var krpano = document.getElementById('krpanoSWFObject');
+    krpano.call("skin_showthumbs();");
+};
+//#endregion
---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //读取文本文件中的字段盘段是否添加了特效
+            ViewBag.Data = Read();
+            return View();
+        }
+        /// <summary>
+        /// 测试添加效果
+        /// </summary>
+        /// <param name="islittleplanet">小行星开场</param>
+        /// <param name="effect">特效设置</param>
+        /// <param name="bgmusic">背景音乐</param>
+        /// <param name="multiscene">多个场景缩略图选择</param>
+        /// <param name="voice">背景解说</param>
+        /// <param name="autorotate">是否自动旋转</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Save(bool islittleplanet, string effect,bool bgmusic,bool multiscene,bool voice,bool autorotate)
+        {
+            if (islittleplanet)
+            {
+                Write("true", effect);
+            }
+            else
+            {
+                Write("false", effect);
+            }
+            return Json(new { status = "ok" });
+        }
+
+
+
+        /// <summary>
+        /// 网站根目录下面保存Data
+        /// </summary>
+        /// <param name="AccessToken"></param>
+        public void Write(string islittleplanet, string effect)
+        {
+            if (System.IO.File.Exists(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt"))
+            {
+                System.IO.File.Delete(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt");
+                System.IO.File.WriteAllText(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt", islittleplanet+"," +effect);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt", islittleplanet + "," + effect);
+            }
+        }
+        /// <summary>
+        /// 读取网站根目录下的Data
+        /// </summary>
+        /// <returns></returns>
+        public string Read()
+        {
+            string path=System.Web.HttpRuntime.AppDomainAppPath + "Data.txt";
+            if (System.IO.File.Exists(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt"))
+            {
+                return System.IO.File.ReadAllText(System.Web.HttpRuntime.AppDomainAppPath + "Data.txt");
+            }
+            return "";
+        }
+    }
+}
